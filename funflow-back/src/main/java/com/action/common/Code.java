29b3a0cdb7cc7c811b141b059b48c0a3c95b3dc9
xiangfu <liuxiangfu2002@163.com>
2025-11-24 16:44:10 +0800

    chore: update code annotation

--- funflow-back/src/main/java/com/action/common/Code.java
+++ funflow-back/src/main/java/com/action/common/Code.java
@@ -8,16 +8,26 @@ package com.action.common;
  */
 public class Code {
 
-    /** 成功 */
+    /**
+     * 默认请求成功状态码
+     */
     public static final int SUCCESS = 200;
-    /** 失败 */
+    /**
+     * 默认请求失败状态吗
+     */
     public static final int ERROR = 400;
-    /** 参数校验失败 */
+    /**
+     * 参数校验失败
+     */
     public static final int VALIDATION_ERROR = 401;
-    /** 业务逻辑异常 */
+    /**
+     * 业务逻辑异常
+     */
     public static final int BUSINESS_ERROR = 402;
-    /** 默认系统异常 */
+    /**
+     * 系统异常
+     */
     public static final int SYSTEM_ERROR = 500;
-    
+
 }
 
--- funflow-back/src/main/java/com/action/common/Result.java
+++ funflow-back/src/main/java/com/action/common/Result.java
@@ -11,14 +11,20 @@ import lombok.Data;
  */
 @Data
 public class Result<T> {
-    
-    /** 状态码 */
+
+    /**
+     * 状态码
+     */
     private Integer code;
-    
-    /** 消息 */
+
+    /**
+     * 消息
+     */
     private String message;
-    
-    /** 返回数据 */
+
+    /**
+     * 返回数据
+     */
     private T data;
     
 
