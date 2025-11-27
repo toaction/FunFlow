1e3ea4cec50ef8ad5a07494edc46b1b33187f503
xiangfu <liuxiangfu2002@163.com>
2025-11-27 23:12:46 +0800

    chore: split oss-service to interface and impl

---
+++ funflow-back/src/main/java/com/action/service/impl/OssServiceImpl.java
@@ -0,0 +1,55 @@
+package com.action.service.impl;
+
+import com.action.config.OssConfig;
+import com.action.service.OssService;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * 阿里云 OSS 服务实现类
+ * 处理文件上传到阿里云对象存储服务
+ *
+ * @author Xiangfu
+ */
+@Service
+public class OssServiceImpl implements OssService {
+
+    @Autowired
+    private OssConfig ossConfig;
+
+    /**
+     * 使用阿里云 OSS 上传图片
+     *
+     * @param file 图片文件
+     * @param fileName 文件名
+     * @return 图片 URL
+     * @throws Exception 上传异常
+     */
+    @Override
+    public String uploadImage(MultipartFile file, String fileName) throws Exception {
+        // 创建 OSSClient 实例
+        OSS ossClient = new OSSClientBuilder().build(
+            ossConfig.getEndpoint(),
+            ossConfig.getAccessKeyId(),
+            ossConfig.getAccessKeySecret()
+        );
+
+        try {
+            // 指定上传路径（可选目录，如 images/）
+            String objectName = "images/" + fileName;
+            // 上传文件流
+            ossClient.putObject(ossConfig.getBucketName(), objectName, file.getInputStream());
+            // 返回文件访问 URL（公共读 bucket）
+            return "https://" + ossConfig.getBucketName() + "." + ossConfig.getEndpoint() + "/" + objectName;
+
+        } finally {
+            // 关闭 OSSClient
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
+    }
+}
